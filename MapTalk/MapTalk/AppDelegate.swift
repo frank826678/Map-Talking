7ae1a7cd2f85ec0e4f8c05affd60f9eaeb7e19cc
Frank <frank826678@gmail.com>
2018-09-20 10:22:29 +0800

    add Fb & Firebase login

---
+++ MapTalk/MapTalk/AppDelegate.swift
@@ -0,0 +1,74 @@
+//
+//  AppDelegate.swift
+//  MapTalk
+//
+//  Created by Frank on 2018/9/19.
+//  Copyright © 2018 Frank. All rights reserved.
+//
+
+import UIKit
+import FBSDKLoginKit
+import FBSDKCoreKit
+import Firebase
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
+        FBSDKSettings.setAppID("242593443093281")
+        
+        //let userDefault = UserDefaults.standard
+        //let userFBToken = userDefault.string(forKey: "userFbTokenFile")
+        
+        //print("給我userToken\(userFBToken)")
+        
+//        if userFBToken != nil {
+//            switchHomePage()
+//        } else {
+//            switchLogin()
+//        }
+       
+        FirebaseApp.configure()
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+        
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+       
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        FBSDKAppEvents.activateApp()
+    }
+    
+    func application(_ app: UIApplication,
+                     open url: URL,
+                     options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
+        let result = FBSDKApplicationDelegate.sharedInstance().application(app, open: url, options: options)
+        return result
+    }
+    
+    func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+        let result = FBSDKApplicationDelegate.sharedInstance().application(
+            application, open: url, sourceApplication: sourceApplication,
+            annotation: annotation)
+        return result
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+
+    }
+    
+}
